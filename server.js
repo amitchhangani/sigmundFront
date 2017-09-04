142a1c2fc5f268129f1153c6fbd046c1acb18f68
Amit Chhangani <chhangani.amit@gmail.com>
2017-09-04 11:08:07 +0530

    Runnig over https

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
+var https = require('https');
+
+var privateKey  = fs.readFileSync('./certs/stagingsdei_com.key', 'utf8');
+var certificate = fs.readFileSync('./certs/c86aaff33f318ca4.crt', 'utf8');
+var ca = fs.readFileSync('./certs/gd_bundle-g2-g1.crt');
+var request = require('request');
+var httpsOptions = {key: privateKey, cert: certificate, ca: ca};
+
+
+var httpsServer = https.createServer(httpsOptions, app);
 
 app.use(express.static(__dirname + '/dist' ) )
 
 app.get('*', function(req,res){
   	res.sendFile("index.html", { root: __dirname+'/dist' })
 })
-app.listen(4102);
+httpsServer.listen(443);
